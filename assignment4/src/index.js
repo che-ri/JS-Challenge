8be4a0d80c484157b67ef4959796826ed423c501
che-ri <rnjs1135@gmail.com>
2021-03-11 17:30:27 +0900

    fix  script

--- assignment4/src/index.js
+++ assignment4/src/index.js
@@ -1,6 +1,3 @@
-// <⚠️ DONT DELETE THIS ⚠️>
-import "./styles.css";
-// <⚠️ /DONT DELETE THIS ⚠️>
 const body = document.querySelector("body");
 const text = document.querySelector(".text");
 const TEXT = "text";
